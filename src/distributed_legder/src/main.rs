a0f2946470b4d8241e3f4a3edad2ef236d9eca34
Junior Monteiro <junior.monteiro@powersolution.cv>
2023-03-20 11:31:15 +0000

    distributed-legder: project estructured

---
+++ src/distributed_legder/src/main.rs
@@ -0,0 +1,10 @@
+extern crate distributed_legder;
+
+use distributed_legder::network::server::Server;
+
+
+fn main() {
+    println!("Hello, world!");
+    Server::print();
+    Server::mytest();
+}
---
+++ src/distributed_legder/src/network/server.rs
@@ -0,0 +1,10 @@
+
+
+pub struct Server{}
+
+impl Server {
+    pub fn print() {
+        println!("rest");
+    }
+
+}
