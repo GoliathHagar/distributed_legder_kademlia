9bf1fe5b0ef2b213260585f1dee4354ee5a1547c
Fabiana Alves <fabs.trab95@gmail.com>
2023-05-20 16:18:19 +0100

    distributed_ledger:update transaction.rs

--- src/distributed_legder/src/blockchain/transaction.rs
+++ src/distributed_legder/src/blockchain/transaction.rs
@@ -3,6 +3,9 @@ use serde::{Serialize, Deserialize};
 /// Represents a transaction in the blockchain.
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Transaction {
+    /// The unique ID of the transaction.
+    pub id: String,
+
     /// The sender of the transaction.
     pub sender: String,
 
@@ -12,5 +15,6 @@ pub struct Transaction {
     /// The amount of the transaction.
     pub amount: f64,
 
-    
+    /// The signature of the transaction signed by the auction owner.
+    pub signature: String,
 }
--- src/distributed_legder/src/lib.rs
+++ src/distributed_legder/src/lib.rs
@@ -6,4 +6,5 @@ pub mod constants;
 pub mod dht;
 pub mod network;
 pub mod blockchain;
+//pub mod auctions;
 pub mod auctions;
