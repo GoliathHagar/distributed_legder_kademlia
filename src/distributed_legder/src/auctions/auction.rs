882e347c606c412895c9b2d0591f7144d4aa40a0
Fabiana Alves <fabs.trab95@gmail.com>
2023-05-20 23:30:48 +0100

    distributed_ledger:update auctions - added auction.rs and bid.rs

--- src/distributed_legder/src/auctions/auction.rs
+++ src/distributed_legder/src/auctions/auction.rs
@@ -1,94 +1,118 @@
-/*
+use std::io::{self, BufRead};
 
-pub struct Auction {
-    auction_id: [u8; 20],
-    auctioneer_node_id: Vec<u8>,
-    auction_name: String,
-    minimum_bid: f32,
-    auction_duration: u64, // in minutes
-    initial_ts: u64,
-    auctioneer_pk: Vec<u8>,
-    signature: Option<Vec<u8>>,
+pub struct AuctionUI {
+    stdin: io::Stdin,
+    max_rows: i32,
 }
 
-impl Auction {
-    pub fn new(
-        auction_name: String,
-        auctioneer_node_id: Vec<u8>,
-        minimum_bid: f32,
-        auction_duration: u64,
-        auctioneer_pk: Vec<u8>,
-        signature: Option<Vec<u8>>,
-    ) -> Self {
-        let initial_ts = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .expect("Failed to obtain system time")
-            .as_secs();
-
-        let auction_id = Self::generate_id(&auction_name, initial_ts);
-
-        Auction {
-            auction_id,
-            auctioneer_node_id,
-            auction_name,
-            minimum_bid,
-            auction_duration,
-            initial_ts,
-            auctioneer_pk,
-            signature,
+impl AuctionUI {
+    pub fn new() -> AuctionUI {
+        AuctionUI {
+            stdin: io::stdin(),
+            max_rows: 3,
         }
     }
 
-    pub fn get_final_ts(&self) -> u64 {
-        self.initial_ts + self.auction_duration
+    pub fn main_menu(&self) -> i32 {
+        println!("1) New auction");
+        println!("2) Find auctions");
+        println!("3) Exit");
+        print!("Choice: ");
+        io::stdout().flush().unwrap();
+
+        let mut choice = String::new();
+        self.stdin.read_line(&mut choice).unwrap();
+
+        let choice = choice.trim().parse().unwrap_or(0);
+
+        while choice <= 0 || choice > self.max_rows {
+            print!("Bad option, choose again: ");
+            io::stdout().flush().unwrap();
+
+            let mut choice = String::new();
+            self.stdin.read_line(&mut choice).unwrap();
+            let choice = choice.trim().parse().unwrap_or(0);
+        }
+
+        choice
     }
 
-    fn generate_id(auction_name: &str, initial_ts: u64) -> [u8; 20] {
-        let ts = initial_ts.to_string();
-        let merge = [auction_name.as_bytes(), ts.as_bytes()].concat();
-        let mut hasher = Sha1::new();
-        hasher.input(&merge);
-        let result = hasher.result();
+    pub fn new_auction_menu(&self) {
+        println!("** MENU TO INITIALIZE AN AUCTION **\n");
 
-        let mut auction_id = [0u8; 20];
-        auction_id.copy_from_slice(&result[..20]);
+        print!("Name of item: ");
+        io::stdout().flush().unwrap();
+        let mut name = String::new();
+        self.stdin.read_line(&mut name).unwrap();
 
-        auction_id
+        print!("Open bid: ");
+        io::stdout().flush().unwrap();
+        let mut opening_bid = String::new();
+        self.stdin.read_line(&mut opening_bid).unwrap();
+        let opening_bid = opening_bid.trim().parse().unwrap_or(0);
+
+        print!("Minimum bid: ");
+        io::stdout().flush().unwrap();
+        let mut minimum_bid = String::new();
+        self.stdin.read_line(&mut minimum_bid).unwrap();
+        let minimum_bid = minimum_bid.trim().parse().unwrap_or(0);
+
+        print!("Auction duration (in min): ");
+        io::stdout().flush().unwrap();
+        let mut auction_duration = String::new();
+        self.stdin.read_line(&mut auction_duration).unwrap();
+        let auction_duration = auction_duration.trim().parse().unwrap_or(0);
+
+        // TODO: Create a new auction with the provided details
     }
 
-    pub fn add_to_signature(&self, signature: &mut Signature) {
-        signature.update(self.auction_name.as_bytes());
-        signature.update(&self.auction_id);
-        signature.update(&self.auctioneer_node_id);
+    pub fn ongoing_auction(&self) {
+        println!("** LIST OF CURRENT AUCTIONS **\n");
+        let auctions: Vec<Auction> = Vec::new();
+        // TODO: Retrieve the list of auctions from the blockchain
 
-        let mut auction = Vec::new();
-        auction.extend_from_slice(&self.minimum_bid.to_be_bytes());
-        auction.extend_from_slice(&self.initial_ts.to_be_bytes());
-        auction.extend_from_slice(&self.auction_duration.to_be_bytes());
+        let mut cont = 1;
+        for auction in &auctions {
+            println!("{} ) {}", cont, auction.get_auction_name());
+            cont += 1;
+        }
 
-        signature.update(&auction);
+        print!("\nChoose: ");
+        io::stdout().flush().unwrap();
+
+        let mut choice = String::new();
+        self.stdin.read_line(&mut choice).unwrap();
+        let mut choice = choice.trim().parse().unwrap_or(0);
+
+        while choice <= 0 || choice >= cont {
+            print!("Bad option, choose again: ");
+            io::stdout().flush().unwrap();
+            let mut choice = String::new();
+            self.stdin.read_line(&mut choice).unwrap();
+            let choice = choice.trim().parse().unwrap_or(0);
+        }
+
+        self.bid_auction(&auctions[choice - 1]);
     }
 
-    pub fn initialize_new_auction(
-        node: &P2PNode,
-        auction_name: String,
-        duration_in_millis: u64,
-        min_bid: f32,
-        owner_keys: &KeyPair,
-    ) -> Self {
-        let auction = Auction::new(
-            auction_name,
-            node.node_id.clone(),
-            min_bid,
-            duration_in_millis,
-            owner_keys.public_key.clone(),
-            None,
-        );
+    pub fn bid_auction(&self, chosen_auction: &Auction) {
+        println!("** MENU TO BID THE ITEM **\n");
 
-        let signature = Signable::calculate_signature_of(&auction, &owner_keys.private_key);
-        auction.signature = Some(signature);
+        println!("Name of the item: {}", chosen_auction.get_auction_name());
+        println!("Starting bid of the item: {}", chosen_auction.get_minimum_bid());
 
-        auction
+        let initial_date = chosen_auction.get_initial_ts();
+        let final_date = chosen_auction.get_final_ts();
+        println!("Start of auction: {}", initial_date);
+        println!("End of auction: {}", final_date);
+
+        print!("\nPlease, choose a bid amount: ");
+        io::stdout().flush().unwrap();
+
+        let mut amount = String::new();
+        self.stdin.read_line(&mut amount).unwrap();
+        let amount = amount.trim().parse().unwrap_or(0.0);
+
+        // TODO: Create a new bid with the chosen auction and bid amount
     }
 }
-*/
--- src/distributed_legder/src/lib.rs
+++ src/distributed_legder/src/lib.rs
@@ -6,5 +6,4 @@ pub mod constants;
 pub mod dht;
 pub mod network;
 pub mod blockchain;
-//pub mod auctions;
 pub mod auctions;
