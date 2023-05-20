7a7bdb5a10074ce19c7628c1b74d1355f94fbfb2
Fabiana Alves <fabs.trab95@gmail.com>
2023-05-20 17:00:01 +0100

    distributed_ledger:update transaction.rs

--- src/distributed_legder/src/blockchain/transaction.rs
+++ src/distributed_legder/src/blockchain/transaction.rs
@@ -1,4 +1,7 @@
 use serde::{Serialize, Deserialize};
+use ring::digest;
+use ring::rand::SystemRandom;
+use ring::signature::{self, KeyPair};
 
 /// Represents a transaction in the blockchain.
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -16,7 +19,7 @@ pub struct Transaction {
     pub amount: f64,
 
     /// The signature of the transaction signed by the auction owner.
-    pub signature: String,
+    pub signature: Option<String>,
 }
 
 impl Transaction {
@@ -27,7 +30,37 @@ impl Transaction {
             sender,
             recipient,
             amount,
-            signature,
+            signature: None,
         }
+    }
+
+    /// Calculates the ID of the transaction as a hash of the sender, recipient, and amount.
+    fn calculate_id(sender: &str, recipient: &str, amount: f64) -> String {
+        let mut hasher = Sha256::new();
+        let data = format!("{}{}{}", sender, recipient, amount);
+        hasher.update(data.as_bytes());
+        let hash = hasher.finalize();
+        hex::encode(hash)
+    }
+
+    /// Signs the transaction based on the hash of the ID, sender, recipient, and amount.
+    pub fn sign(&mut self) {
+        let data = format!("{}{}{}{}", self.id, self.sender, self.recipient, self.amount);
+        let signature = Transaction::calculate_signature(&data);
+        self.signature = Some(signature);
+    }
+
+    /// Calculates the signature of the transaction data.
+    fn calculate_signature(data: &str) -> String {
+        // Generate a new key pair
+        let rng = SystemRandom::new();
+        let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rng).unwrap();
+        let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
+
+        // Sign the data
+        let signature = key_pair.sign(data.as_bytes());
+
+        // Convert the signature to a hexadecimal string
+        hex::encode(signature.as_ref())
     }
 }
